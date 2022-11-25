0ccc44786d227782a775c8f9648974e5d6a93e65
BibiiXD <gabimoliveiraah@gmail.com>
2022-11-25 17:37:46 +0000

    Apagar

--- livros_delete.php
+++ livros_delete.php
@@ -1,31 +1,35 @@
 <?php
-
 require_once './vendor/autoload.php';
 
-use  ExemploPDOMySQL\MySQLConnection;
+use ExemploPDOMySQL\MySQLConnection;
 
 $bd = new MySQLConnection();
-
 $livro = null;
 
 if($_SERVER['REQUEST_METHOD'] == 'GET') {
-$comando = $bd->prepare('SELECT * FROM livros WHERE id = :id');
-$comando->execute([]);
-
-$generos = $comando->fetchAll(PDO::FETCH_ASSOC);
-
-$comandoLivro = $bd->prepare('SELECT * FROM livros WHERE id = :id');
-$comandoLivro->execute([':id' => $_GET['id']]);
-$livro = $comandoLivro->fetch(PDO::FETCH_ASSOC);
+    $comando = $bd->prepare('SELECT * FROM livros WHERE id = :id');
+    $comando->execute([':id' => $_GET['id']]);
+    $livro = $comando->fetch(PDO::FETCH_ASSOC);
 } else {
-    $comando = $bd->prepare('UPDATE livros SET titulo = :titulo, id_genero = :genero WHERE id = :id'); 
-    $comando->execute([':titulo' => $_POST['titulo'], ':genero' => $_POST['genero'], ':id' => $_POST['id']]);
+    $comando = $bd->prepare('DELETE FROM livros WHERE id = :id');
+    $comando->execute([':id' => $_POST['id']]);
 
     header('Location:/livros_list.php');
 }
+
+$_title = 'Remover Livro';
+
 ?>
 
 <?php include('./includes/header.php') ?>
 
-<h1>Remover Livro</h1>
-<p>Tem certeza que deseja excluir </p>
+            <h1>Remover Livro</h1>
+            <p>Tem certeza que deseja excluir o livro <?= $livro['titulo'] ?>?</p>
+
+            <form action="livros_delete.php" method="post">
+                <input type="hidden" name="id" value="<?= $livro['id'] ?>" />
+                <a class="btn btn-secondary" href="livros_list.php">Voltar</a>
+                <button class="btn btn-danger" type="submit">Excluir</button>
+            </form>
+            
+<?php include('./includes/footer.php') ?>
